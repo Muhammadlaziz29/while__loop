9984b015d156807cca82dc66ef0a1c530f71b384
Muhammadlaziz29 <muxammadlaziz29@gmail.com>
2022-08-29 13:33:56 +0500

    hello

--- bin/while__loop22.dart
+++ bin/while__loop22.dart
@@ -1,3 +1,10 @@
 void main() {
   //Print the product of odd numbers from 123 to 500
+  int m = 1;
+  for (int i = 123; i <= 500; i++) {
+    if (i % 2 == 1) {
+      m *= i;
+    }
+  }
+  print(m);
 }
