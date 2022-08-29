25a75e32d90dbb0fdec6ca16ed695c6fc8dad158
Muhammadlaziz29 <muxammadlaziz29@gmail.com>
2022-08-29 13:30:47 +0500

    hello

--- bin/while__loop19.dart
+++ bin/while__loop19.dart
@@ -1,3 +1,10 @@
 void main() {
   //Print the sum of odd numbers between 50 and 1000
+  int m = 0;
+  for (int i = 50; i <= 1000; i++) {
+    if (i % 2 == 1) {
+      m += i;
+    }
+  }
+  print(m);
 }
