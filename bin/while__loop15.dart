798297cd20456041872d4398212eb04a28fb1766
Muhammadlaziz29 <muxammadlaziz29@gmail.com>
2022-08-29 13:28:21 +0500

    hello

--- bin/while__loop15.dart
+++ bin/while__loop15.dart
@@ -1,3 +1,10 @@
 void main() {
   //Print the sum of pairs of numbers from 1 to 100
+  int m = 0;
+  for (int i = 0; i <= 100; i++) {
+    if (i % 2 == 0) {
+      m += i;
+    }
+  }
+  print(m);
 }
