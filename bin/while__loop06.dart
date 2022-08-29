b0b99b15677149feb7f3d47db1f7288a69c5c5f8
Muhammadlaziz29 <muxammadlaziz29@gmail.com>
2022-08-29 13:15:33 +0500

    hello

--- bin/while__loop06.dart
+++ bin/while__loop06.dart
@@ -1,3 +1,9 @@
+
 void main() {
-  //Print the odd numbers from 1 to 100
+   //Print the odd numbers from 1 to 100
+  for (int i = 1; i <= 100; i++) {
+    if (i % 2 == 1) {
+      print(i);
+    }
+  }
 }
