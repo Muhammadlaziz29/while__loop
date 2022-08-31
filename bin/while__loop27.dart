0578165bb2768648dd64fa0c31d84ecd32bcb367
Muhammadlaziz29 <muxammadlaziz29@gmail.com>
2022-08-31 12:53:01 +0500

    hello

--- bin/while__loop27.dart
+++ bin/while__loop27.dart
@@ -1,3 +1,13 @@
 void main() {
   //Print the first odd numbers and then the even numbers from 100 to 1000.
+  for(int i = 100; i<1000; i++) {
+    if(i%2==1) {
+      print(i);
+    }
+  }
+  for(int i = 100; i<1000; i++) {
+    if(i%2==0) {
+      print(i);
+    }
+  }
 }
