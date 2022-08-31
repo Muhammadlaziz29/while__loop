4e4754daf7f4ee763659fa7abe0d16e89f4fac1a
Muhammadlaziz29 <muxammadlaziz29@gmail.com>
2022-08-31 13:08:25 +0500

    hello

--- bin/while__loop34.dart
+++ bin/while__loop34.dart
@@ -1,3 +1,12 @@
 void main() {
   //Add odd numbers from 1 to 100 to the list. And print the list
+    List a = [];
+  for(int i = 1; i<=100;i++) {
+    if(i%2==1) {
+       a.add(i);
+    }
+   
+    
+  }
+  print(a);
 }
