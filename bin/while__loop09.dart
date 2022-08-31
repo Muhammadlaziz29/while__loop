ca044ee79bac3394e56ed654ff7e7df4076fbc15
Muhammadlaziz29 <muxammadlaziz29@gmail.com>
2022-08-31 10:42:29 +0500

    hello

--- bin/while__loop09.dart
+++ bin/while__loop09.dart
@@ -1,7 +1,6 @@
-
 void main() {
-   //Print pairs of numbers from 100 to 1000
-  for (int i = 100; i <= 1000; i++) {
+  //Print pairs of numbers from 100 to 1000
+  for (int i = 100; i < 1000; i++) {
     if (i % 2 == 0) {
       print(i);
     }
