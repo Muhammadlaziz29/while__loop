0cee603f6d5b3e01bb4ee0c44599fb0a5d95a409
Muhammadlaziz29 <muxammadlaziz29@gmail.com>
2022-08-31 11:00:28 +0500

    hello

--- bin/while__loop18.dart
+++ bin/while__loop18.dart
@@ -1,7 +1,7 @@
 void main() {
   //Print the sum of pairs of numbers between 50 and 1000
   int m = 0;
-  for (int i = 50; i <= 1000; i++) {
+  for (int i = 50; i < 1000; i++) {
     if (i % 2 == 0) {
       m += i;
     }
