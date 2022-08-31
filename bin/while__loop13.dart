a8da7c34cb7106e3503d2212fab2d95a59c93980
Muhammadlaziz29 <muxammadlaziz29@gmail.com>
2022-08-31 11:13:54 +0500

    hello

--- bin/while__loop13.dart
+++ bin/while__loop13.dart
@@ -1,6 +1,6 @@
 void main() {
   //Print your name and age 100 times, not two on the same line
-  for (int i = 0; i<=100; i++) {
+  for (int i = 0; i<100; i++) {
     print('Muhammadlaziz');
     print('20');
   }
