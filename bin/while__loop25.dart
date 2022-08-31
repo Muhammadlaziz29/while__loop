bb0085c0fbf6fcde1031e1d1dcd36705e2d8efdd
Muhammadlaziz29 <muxammadlaziz29@gmail.com>
2022-08-31 12:43:48 +0500

    hello

--- bin/while__loop25.dart
+++ bin/while__loop25.dart
@@ -1,6 +1,6 @@
 void main() {
   //Print the first even numbers from 1 to 100 and the second odd numbers
-  for (int i = 1; i < 100; i++) {
+  for (int i = 0; i < 100; i++) {
     if (i % 2 == 0) {
       print(i);
     }
